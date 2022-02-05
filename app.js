1dc9aa287f76c088065a89e7c2277ffd44e3080e
Fishcomeback <fernandocabred@hotmail.com>
2022-02-05 02:27:36 -0300

    prueba para heroku

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ app.listen(process.env.PORT || 3000, function () {
     console.log('servidor corriendo en puerto 3000')
 })
 
-const publicPath = path.resolve(__dirname, 'public')
-app.use(express.static(publicPath))
+//const publicPath = path.resolve(__dirname, 'public')
+//app.use(express.static(publicPath))
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')))
+//app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')))
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
